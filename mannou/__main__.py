ecb1c6cd768d702e1b601cef2d9e69db50b34ab1
Muhammad Adi Prasojo <borderlineargs@gmail.com>
2018-10-19 22:02:59 +0700

    Add main doc

--- mannou/__main__.py
+++ mannou/__main__.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Make module executable.
+
+Everytime you use this command::
+
+    $ python3 -m mannou
+
+This is the first module that executed.
+
+
+.. _Python `__main__` documentation:
+    https://docs.python3.org/3/library/__main__.html
+
+"""
+
 from . import cli
 
 cli.main()
