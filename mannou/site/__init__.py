9cfc6798791a135b7571c769ddf8030e960b81a1
Muhammad Adi Prasojo <borderlineargs@gmail.com>
2018-10-21 23:22:42 +0700

    Fix rst error.

--- mannou/site/__init__.py
+++ mannou/site/__init__.py
@@ -12,10 +12,10 @@ have the same functionality.
 Guideline
 ---------
 * Make sure parser class inherit `mannou.parser.Manga`.
-* Implement all abstract method
-* All implemented abstract method **MUST** return
-  an expected object specified in `super().__doc__
-* It strongly recommended if `number` attribute
+* Implement all abstract method.
+* All implemented abstract method **MUST** return.
+  an expected object specified in `super().__doc__`.
+* It strongly recommended if `number` attribute.
   from :obj:`mannou.parser.Chapter` is a cardinal number.
 * You must override `super().filter_chapters()` if the parse do not adhere
   rule above.
